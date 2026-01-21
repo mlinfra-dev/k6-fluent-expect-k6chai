fd72fb7552737183cbebad72e6532a247119891e
Krishna Kant Sharma <kksharma1618@gmail.com>
2026-01-21 19:40:28 +0530

    dont package @types/chai

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ export { configureAssertOverride };
 export interface Assertion extends Chai.Assertion {
 }
 
-// 2. Override expect to return YOUR interface
 export const expect = (val: any, message?: string): Assertion => {
     return chai.expect(val, message) as unknown as Assertion;
 };
