ad18b549999c799a1484ebd0e8db2cd9c9d4e333
Krishna Kant Sharma <kksharma1618@gmail.com>
2026-01-28 15:01:08 +0530

    add comments to config

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,37 @@ export default chai;
 export { configureAssertOverride };
 
 export interface Config extends Chai.Config {
+  /**
+   * Variables interpolated into check message are truncated to this length. It prevents mistakes when the check name is very large, especially when aggregateChecks is false.
+   *
+   * Default: 100
+   */
   truncateVariableThreshold: number;
+  /**
+   * Check message length is truncated to this value.
+   *
+   * Default: 300
+   */
   truncateMsgThreshold: number;
+  /**
+   * The actual values are not interpolated into the check message. Disable for tests with 1 iteration.
+   *
+   * Default: true
+   */
   aggregateChecks: boolean;
+  /**
+   * When the check fails, debug messages are printed.
+   *
+   * Default: false
+   */
   logFailures: boolean;
+  /**
+   * Aborts the test entirely
+   *
+   * Calls `exec.test.abort(truncatedExpectation);`, where `exec is 'k6/execution' module`
+   *
+   * Default: false
+   */
   exitOnError: boolean;
 }
 
